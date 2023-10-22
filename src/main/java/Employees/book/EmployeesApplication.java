90af532cf584171d471aa7b38fd148c8902f3020
Jonathan-sm <Jonathan_sm@mail.ru>
2023-10-22 21:57:38 +0300

    создаем

---
+++ src/main/java/Employees/book/EmployeesApplication.java
@@ -0,0 +1,13 @@
+package Employees.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesApplication.class, args);
+	}
+
+}
